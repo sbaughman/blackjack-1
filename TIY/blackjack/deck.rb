8ab2d9f9d289a2f5bf0ac562e5463b194829fd5b
Seth Baughman <sbaughman1@me.com>
2016-03-14 13:26:53 -0400

    Move face to value map out of initialize

--- TIY/blackjack/deck.rb
+++ TIY/blackjack/deck.rb
@@ -1,17 +1,17 @@
 require_relative 'card'
 
 class Deck < Array
-  attr_accessor :card_key
-
   def initialize
-    self.card_key = {2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 10 => 10, "Jack" => 10, "Queen" => 10, "King" => 10, "Ace" => 11}
     fill_suit("Hearts")
     fill_suit("Diamonds")
     fill_suit("Clubs")
     fill_suit("Spades")
   end
 
-  # @param [Object] name # <-- Why does Ruby Mine think I might want this?
+  def card_key
+    {2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 10 => 10, "Jack" => 10, "Queen" => 10, "King" => 10, "Ace" => 11}
+  end
+
   def fill_suit(suit="hearts")
     card_key.each_pair { |name, value| self << Card.new(value, suit, name.to_s) }
   end
